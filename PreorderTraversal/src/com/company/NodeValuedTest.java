d5db612ca7b9cfab8582e487a82a0850e8f6405d
NicholasDamian <nicholas4hall@gmail.com>
2017-06-01 15:03:47 -0400

    First NonReapeated Char in a String

--- PreorderTraversal/src/com/company/NodeValuedTest.java
+++ PreorderTraversal/src/com/company/NodeValuedTest.java
@@ -11,7 +11,13 @@ import static org.junit.Assert.*;
 public class NodeValuedTest {
     @Before
     public void setUp() throws Exception {
-
+        NodeValued<Integer> node12 = new NodeValued<>(null,null,12);
+        NodeValued<Integer> node8 = new NodeValued<>(null,null,8);
+        NodeValued<Integer> node6 = new NodeValued<>(null,null,6);
+        NodeValued<Integer> node4 = new NodeValued<>(null,null,4);
+        NodeValued<Integer> node10 = new NodeValued<>(node8,node12,10);
+        NodeValued<Integer> node5 = new NodeValued<>(node4,node6,5);
+        NodeValued<Integer> root = new NodeValued<>(node5,node10,7);
     }
 
 
