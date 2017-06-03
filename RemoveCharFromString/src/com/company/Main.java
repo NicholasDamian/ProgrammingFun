8f82732ab936fb0919e9f517b0e0157fad80eefe
NicholasDamian <nicholas4hall@gmail.com>
2017-06-03 19:28:54 -0400

    Can't remember the next chall

---
+++ RemoveCharFromString/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+//    public String RemoveCharFromString(String str, String remove){
+//
+////        One position for comparing and One position for rewriting
+//
+//        for()
+//
+//
+//        return null;
+//    }
+
+    public static void main(String[] args) {
+
+
+        System.out.print("Battle of the Vowels:Hawaii vs Gronzy".replaceAll("[aeiou]",""));
+
+    }
+}
